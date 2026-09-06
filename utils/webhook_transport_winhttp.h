a2347db646a4de3d7d19948842f94fc2fe7e0b95
maffemuis <55630118+maffemuis@users.noreply.github.com>
2026-09-07 00:07:32 +0200

    Bound direct WinHTTP webhook payloads

--- utils/webhook_transport_winhttp.h
+++ utils/webhook_transport_winhttp.h
@@ -30,7 +30,7 @@ public:
 
     static bool IsSafeBearerToken(const std::string& token);
     static bool IsSafeCredentialTarget(const std::wstring& target_name);
-    static bool IsRepresentableBodySize(std::size_t body_size);
+    static bool IsSafeBodySize(std::size_t body_size);
 };
 
 // Reads a Windows Credential Manager GENERIC credential by target name.
